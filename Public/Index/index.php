1d3f34e8ecf8a163e1909b28977dd51efa9ab9df
Anderson <93753145+Anderr009@users.noreply.github.com>
2022-05-05 01:48:41 -0400

    prueba

--- Public/Index/index.php
+++ Public/Index/index.php
@@ -13,15 +13,7 @@
     </head>
     <body>
         
-        <?php 
-     if(isset($_SESSION['estado'])){ 
-         include("../../includes/headerR.php");
-        }else{
-        include("../../includes/header-NR.php");
 
-    }
-
-        ?>
 
     <section class="welcome">
             <div class="info">
@@ -30,57 +22,7 @@
             </div>
     </section>
 
-<<<<<<< HEAD
-    <section class="productos">
-            <div class="informacion">
-                    <h2>Productos disponibles</h2>
-            </div>
-            <div class="contenido">
-            <?php
-            if(isset($_SESSION['valorBuscar'])){
-                $_SESSION['valorBuscar']+= 10;
-            }
-            
-            MostrarProductos(10);
-            ?>
-            <?php
-            
-            //funcion para productos
-            function MostrarProductos($id){
-                $conex = new Conexion();
-                $cn = $conex->conexion();
-                $idReducido=$id - 10;
-                $sqlSelect = "SELECT * FROM producto WHERE aprobada = 1";
-                $query = $cn->prepare($sqlSelect);
-                $query->execute();
-                while($registro = $query->fetch(PDO::FETCH_ASSOC)){
-                        ?>
-                                <div class="bloque-prod">
-                                        <div class="img">
-                                                <img src="../../imagenes/productos/<?php echo$registro['foto']?>" width="200px" height="200px" alt="">
-                                        </div>
-                                        <div class="info">
-                                                <h4 class="nombre">
-                                                        <?php echo $registro['Nombre'];?>
-                                                </h4>
-                                                <p class="infoProd">
-                                                        Quedan: <?php echo $registro['Disponible'];?>
-                                                </p>
-                                        </div>
-                                        <div class="precio">
-                                                <?php echo $registro['Precio']."RD";?>
-                                        </div>
-                                        <div class="btn">
-                                            <a href="plantilla-prod.php?id=<?php echo $registro['id']?>">Comprar</a></div>
-                                </div>
-                        <?php
-                }
-            }
-            ?>
-            </div>
-            
-    </section>
-=======
+
     <div class="contenido">
     <?php
 
