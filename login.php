e53225487fa104b527552d544093fb5b2e6450ea
SmokeWeeD1 <dariel0923@outlook.com>
2022-05-05 00:04:52 -0400

    Login con css

--- login.php
+++ login.php
@@ -1,6 +1,7 @@
 <?php session_start(); ?>
 <html>
 	<head>
+		<link rel="stylesheet" href="CSS/login.css">
 		<title>Formulario de Registro</title>
 	</head>
 	<body>
@@ -11,11 +12,13 @@
 
 		<form role="form" name="login" action="app/login.php" method="post">
 		  <div class="form-group">
-		    <label for="username">Nombre de usuario o email</label>
+			  <span></span>
+		    <label for="username">usuario </label>
 		    <input type="text" class="form-control" id="username" name="username" placeholder="Nombre de usuario">
 		  </div>
 		  <div class="form-group">
-		    <label for="password">Contrase&ntilde;a</label>
+			  <span></span>
+		    <label for="password">Contrase&ntilde;a	</label>
 		    <input type="password" class="form-control" id="password" name="password" placeholder="Contrase&ntilde;a">
 			<br>
 			Mantener sesion iniciada<input type="checkbox" name="mantener_sesion_abierta" value="si"><br>
@@ -23,6 +26,9 @@
 
 		  <button type="submit" class="btn btn-primary">Acceder</button>
 		</form>
+		<div class="signup-link">
+			No estas registrado? <a href="registro.php"> signup
+		</div>
 </div>
 </div>
 </div>
