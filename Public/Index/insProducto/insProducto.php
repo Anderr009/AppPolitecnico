00e0b258c8b6c285acec31e9a8fcdef06e96cd5e
JuniorMM0110 <morajunior42@gmail.com>
2022-05-06 08:57:56 -0400

    Cambios

--- Public/Index/insProducto/insProducto.php
+++ Public/Index/insProducto/insProducto.php
@@ -11,10 +11,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Insertar producto</title>
  <link rel="stylesheet" href="../../../CSS/insProducto.css">
+ <link rel="stylesheet" href="../../../CSS/styles.css">
 </head> 
 <body>
     
-
+    
 
     <?php
         if(isset($_POST['Enviar'])){
@@ -23,34 +24,37 @@
           //  $fecha = $_POST['fecha'];
             $disponibles = $_POST['cantidad'];
             $producto = new Producto($nombre,$disponibles,$precio);
-           // $descuento = $_POST[''];
-           session_start();
-           $_SESSION['productoTemp'] = $producto;
-           header("location:insImagen.php");
+            // $descuento = $_POST[''];
+            session_start();
+            $_SESSION['productoTemp'] = $producto;
+            header("location:insImagen.php");
         }
 
     ?>
 
     <!-- Volver al dashboard es temporal a lo que se programa un inventario.-->
-    <a href="../index.php">Volver</a>
+    <a href="../index.php"><button class="botones-azul">Volver</button></a>
     <form action="" method="POST">
-       
-        <label for="Name" class="field">Nombre del Producto</label>
-        <input type="text" class="field "name="nombre" id="">
+        <h2 class="titulo">Publicar</h2>
+        <hr>
+        <div class="cont">
+            <label for="Name" class="field">Nombre del Producto</label>
+            <!-- <label for="Date">fechaIngreso:</label>-->
+            <input type="text" name="fecha" id="">
+        </div>
+<div class="cont">
+    <label for="Price">Precio:</label>
+    <input type="text" name="precio" id="">
+</div>
+<div class="cont">
+    <label for="cantidad">CantidadDISP:</label>
+    <input type="text" name="cantidad" id="">   
+    </div>
 
-       <!-- <label for="Date">fechaIngreso:</label>
-        <input type="text" name="fecha" id="">-->
-
-        <label for="Price">Precio:</label>
-        <input type="text" name="precio" id="">
-
-        <label for="cantidad">CantidadDISP:</label>
-        <input type="text" name="cantidad" id="">
-
-       <!-- <label for="Descuento">Descuento:</label>
-        <input type="text" name="descuento" id="">-->
-
-        <input type="submit" class="btn" value="Enviar" name="Enviar">
+       <label for="Descuento">Descuento:</label>
+        <input type="text" name="descuento" id="">
+<br>
+        <button name="Enviar" class="botones-azul">Enviar</button>
 
     </form>
 </table>
